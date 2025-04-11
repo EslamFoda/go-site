006396cfa30143bfea1e92149f2695c78b0eb5ff
EslamFoda <eslaamfodaa@gmail.com>
2025-04-11 09:09:32 +0200

    fix ui mobile issues on template page

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -10,7 +10,7 @@ async function Page() {
 
   if (!user) redirect("/login");
   return (
-    <div className="container max-w-screen-md space-y-10 mt-28 mx-auto">
+    <div className="container max-w-screen-md space-y-10 mt-28 sm:mb-4 mb-24 mx-auto">
       <div className="flex flex-col mt-10 gap-5 items-center justify-center text-center">
         <h1 className="text-2xl font-bold">AI Business Websites</h1>
         <p className="text-sm text-muted-foreground max-w-[360px]">
@@ -18,7 +18,7 @@ async function Page() {
           templates. No coding or design skills needed.
         </p>
       </div>
-      <TemplateList user={user}/>
+      <TemplateList user={user} />
     </div>
   );
 }
